362045c65305c5cee4b9afd07b7e3b587e11013c
lengfo-art <lengfo@163.com>
2025-04-12 15:44:43 +0800

    第一次提交

--- hello.py
+++
@@ -1,7 +0,0 @@
-'''
-第一个python程序
-@author: 林风
-@time: 2025/04/12 
-'''
-
-print("Hello, world!")    # 输出Hello, world!
---
+++ 初步认识python/hello.py
@@ -0,0 +1,11 @@
+'''
+第一个python程序
+@author: 林风
+@time: 2025/04/12 
+'''
+
+print("Hello, world!")    # 输出Hello, world!
+
+message = "Hello, this is a message!"   # 定义变量message
+print(message)   # 输出变量message
+# 注释：# 后面的内容为注释，不会被执行
